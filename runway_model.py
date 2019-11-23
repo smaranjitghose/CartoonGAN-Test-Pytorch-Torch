3dcaa4e6825ac4c3707fe8f996f0e48fa61a644d
SreeHarshaNelaturu <nelaturu.harsha@gmail.com>
2019-11-23 17:36:37 +0530

    model, requirements and yaml

---
+++ runway_model.py
@@ -0,0 +1,69 @@
+import torch
+import os
+import numpy as np
+import argparse
+from PIL import Image
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torchvision.utils as vutils
+from network.Transformer import Transformer
+import runway
+from runway.data_types import *
+from save_utils import *
+
+@runway.setup(options={"checkpoint" : file(extension=".pth")})
+def setup(opts):
+    model = Transformer()
+    model.load_state_dict(torch.load(opts["checkpoint"]))
+    model.eval()
+
+    if torch.cuda.is_available():
+        print("GPU Mode")
+    else:
+        print("CPU Mode")
+        model.float()
+
+    return model
+
+command_inputs = {"input_image" : image, "load_size" : number(description="Adjust loaded size", default=450, step=10, min=450)}
+command_outputs = {"output_image" : image}
+
+@runway.command("cartoonize_image", inputs=command_inputs, outputs=command_outputs, description="Cartoonize the input image")
+def cartoonize_image(model, inputs):
+
+    img = inputs["input_image"].convert("RGB")
+
+    h, w = img.size[0], img.size[1]
+    ratio = h * 1.0 / w
+    if ratio > 1:
+        h = inputs["load_size"]
+        w = int(h * 1.0/ratio)
+    else:
+        w = inputs["load_size"]
+        h = int(w * 1.0/ratio)
+
+    input_image = img.resize((h, w), Image.BICUBIC)
+    input_image = np.asarray(input_image)
+    # RGB -> BGR
+    input_image = input_image[:, :, [2, 1, 0]]
+    input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+    # preprocess, (-1, 1)
+    input_image = -1 + 2 * input_image 
+    if torch.cuda.is_available():
+        input_image = Variable(input_image, volatile=True).cuda()
+    else:
+        input_image = Variable(input_image, volatile=True).float()
+    # forward
+    output_image = model(input_image)
+    output_image = output_image[0]
+    # BGR -> RGB
+    output_image = output_image[[2, 1, 0], :, :]
+    # deprocess, (0, 1)
+    output_image = output_image.data.cpu().float() * 0.5 + 0.5
+    
+    out = save_image(output_image)
+
+    return {"output_image" : out}
+
+if __name__ == "__main__":
+    runway.run(model_options={"checkpoint" : "./pretrained_model/Hayao_net_G_float.pth"})
--- test.py
+++ test.py
@@ -20,55 +20,56 @@ opt = parser.parse_args()
 
 valid_ext = ['.jpg', '.png']
 
-if not os.path.exists(opt.output_dir): os.mkdir(opt.output_dir)
+#if not os.path.exists(opt.output_dir): os.mkdir(opt.output_dir)
 
 # load pretrained model
 model = Transformer()
 model.load_state_dict(torch.load(os.path.join(opt.model_path, opt.style + '_net_G_float.pth')))
 model.eval()
 
-if opt.gpu > -1:
+if torch.cuda.is_available():
 	print('GPU mode')
 	model.cuda()
 else:
 	print('CPU mode')
 	model.float()
 
-for files in os.listdir(opt.input_dir):
+"""for files in os.listdir(opt.input_dir):
 	ext = os.path.splitext(files)[1]
 	if ext not in valid_ext:
 		continue
-	# load image
-	input_image = Image.open(os.path.join(opt.input_dir, files)).convert("RGB")
-	# resize image, keep aspect ratio
-	h = input_image.size[0]
-	w = input_image.size[1]
-	ratio = h *1.0 / w
-	if ratio > 1:
-		h = opt.load_size
-		w = int(h*1.0/ratio)
-	else:
-		w = opt.load_size
-		h = int(w * ratio)
-	input_image = input_image.resize((h, w), Image.BICUBIC)
-	input_image = np.asarray(input_image)
-	# RGB -> BGR
-	input_image = input_image[:, :, [2, 1, 0]]
-	input_image = transforms.ToTensor()(input_image).unsqueeze(0)
-	# preprocess, (-1, 1)
-	input_image = -1 + 2 * input_image 
-	if opt.gpu > -1:
-		input_image = Variable(input_image, volatile=True).cuda()
-	else:
-		input_image = Variable(input_image, volatile=True).float()
-	# forward
-	output_image = model(input_image)
-	output_image = output_image[0]
-	# BGR -> RGB
-	output_image = output_image[[2, 1, 0], :, :]
-	# deprocess, (0, 1)
-	output_image = output_image.data.cpu().float() * 0.5 + 0.5
-	# save
-	vutils.save_image(output_image, os.path.join(opt.output_dir, files[:-4] + '_' + opt.style + '.jpg'))
+	# load image"""
+input_image = Image.open("lala.jpg").convert("RGB")
+# resize image, keep aspect ratio
+h = input_image.size[0]
+w = input_image.size[1]
+ratio = h *1.0 / w
+if ratio > 1:
+	h = 450
+	w = int(h*1.0/ratio)
+else:
+	w = 450
+	h = int(w * ratio)
+input_image = input_image.resize((h, w), Image.BICUBIC)
+input_image = np.asarray(input_image)
+# RGB -> BGR
+input_image = input_image[:, :, [2, 1, 0]]
+input_image = transforms.ToTensor()(input_image).unsqueeze(0)
+# preprocess, (-1, 1)
+input_image = -1 + 2 * input_image 
+if torch.cuda.is_available():
+	input_image = Variable(input_image, volatile=True).cuda()
+else:
+	input_image = Variable(input_image, volatile=True).float()
+# forward
+output_image = model(input_image)
+output_image = output_image[0]
+# BGR -> RGB
+output_image = output_image[[2, 1, 0], :, :]
+# deprocess, (0, 1)
+output_image = output_image.data.cpu().float() * 0.5 + 0.5
+# save
+#vutils.save_image(output_image, os.path.join(opt.output_dir, files[:-4] + '_' + opt.style + '.jpg'))
+vutils.save_image(output_image, "./stonks.jpg")
 
 print('Done!')
